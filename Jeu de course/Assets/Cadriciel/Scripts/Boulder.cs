88c087ac55aa355a995758a72b432bfdf9f57c84
Seeven <seeven.dev@gmail.com>
2015-12-03 16:16:14 -0500

    [Elements en mouvement] Ajout d'un rocher qui roule en coupant la piste. Importation de squelettes (pour la fin de la piste). [Nouveau tracé] Augmentation du relief autour de la piste et de la bordure externe de la côte.

---
+++ Jeu de course/Assets/Cadriciel/Scripts/Boulder.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boulder : MonoBehaviour
+{
+	// ==========================================
+	// == Attributs
+	// ==========================================
+
+	private Vector3 _initialPosition;
+
+	[SerializeField]
+	private int _initialDownImpulse = 10;
+	
+
+	// ==========================================
+	// == Start
+	// ==========================================
+
+	void Start ()
+	{
+		_initialPosition = transform.position;
+		launch();
+	}
+
+	void launch()
+	{
+		transform.position = _initialPosition;
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.AddForce(Vector3.down * _initialDownImpulse, ForceMode.Impulse);
+	}
+
+	// ==========================================
+	// == Update
+	// ==========================================
+
+	void Update ()
+	{
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.transform.parent != null && col.transform.parent.name == "Tourelle")
+		{
+			launch();
+		}
+	}
+}
---
+++ Jeu de course/Assets/Cadriciel/Scripts/LoopAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopAnimation : MonoBehaviour
+{
+	[SerializeField]
+	private string animationName = "dance";
+
+	private Animation _animation;
+	private bool isRewinding = false;
+
+	void Start ()
+	{
+		_animation = GetComponent<Animation>();
+	}
+	
+	void Update ()
+	{
+		if (!_animation.IsPlaying(animationName))
+		{
+			Debug.Log("recommence");
+			_animation.Play(animationName);
+		}
+	}
+}
