cd92b6df80beb3facf99c3243e4fa7ddfd3fd766
Seeven <seeven.dev@gmail.com>
2015-12-06 22:23:24 -0500

    [Guidage] Correction affichage

--- Jeu de course/Assets/Cadriciel/Scripts/Virage.cs
+++ Jeu de course/Assets/Cadriciel/Scripts/Virage.cs
@@ -1,52 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Virage : MonoBehaviour 
+public class Virage : MonoBehaviour
 {
-	[SerializeField]
-	private GUIText virage;
-	
-	// Use this for initialization
-	void Start () {
+	private DirectionInterface virage;
 
+	[SerializeField]
+	private string virageDirection;
+
+	private string direction = "";
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+		virage = GameObject.Find("VirageIndicator").GetComponent<DirectionInterface>() as DirectionInterface;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		virage.text = getIndication ();
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.transform.parent.parent != null && col.transform.parent.parent.name == "Joueur 1")
+		{
+			virage.setText(virageDirection);
+		}
 	}
-	
-	public string getIndication() {
-		var x = transform.position.x;
-		var z = transform.position.z;
-		
-		//Premier virage
-		if (x > -20 && x < 20 && z > 160 && z < 190) {
-			return "1.LEFT";
+
+	void OnTriggerExit(Collider col)
+	{
+
+		if (col.transform.parent.parent != null && col.transform.parent.parent.name == "Joueur 1")
+		{
+			virage.setText("");
 		}
-		//Deuxième virage
-		else if(x > -250 && x < -130 && z > 20 && z < 100){
-			return "2.RIGHT";
-		}
-		//Troisième virage
-		else if(x > -305 && x < -260 && z > 67 && z < 120){
-			return "3.LEFT";
-		}
-		//Quatrième virage
-		else if(x > -400 && x < -335 && z > -30 && z < 5){
-			return "4.LEFT";
-		}
-		//Cinquième virage
-		else if(x > -255 && x < -200 && z > -130 && z < 60){
-			return "5.LEFT";
-		}
-		//Sixième virage
-		else if(x > -78 && x < -33 && z > -95 && z < -50){
-			return "6.LEFT";
-		}
-		//Pas de virage
-		else{
-			return "";
-		}
+
 	}
 }
---
+++ Jeu de course/Assets/Resources/DirectionInterface.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DirectionInterface : MonoBehaviour
+{
+	private string direction;
+	
+	void Start ()
+	{
+		direction = "";
+	}
+	
+	public void setText(string text)
+	{
+		GetComponent<GUIText>().text = text;
+	}
+}
